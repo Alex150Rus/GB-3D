e93a3bfe9fa76ace80e1c13d22fe853d74217c12
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-01-13 16:21:54 +0300

    PlayerInput script added - beginner, changed PlayerController and made other changes

---
+++ Assets/Scripts/Player/MyCharPhisicsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MyCharPhisicsController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeedMultyplier = 15.0f;
+    
+    private Rigidbody _body;
+
+    private Vector3 _moveDirection;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+        PlayerInput.OnInput += Move;
+        PlayerInput.OnInputSpaceBtn += Jump;
+    }
+
+    private void Update()
+    {
+        //поворачиваем перса;
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _moveDirection, 5f * Time.deltaTime, 0f);
+        transform.rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void Move(Vector3 input)
+    {
+        _moveDirection.Set(input.x * _moveSpeedMultyplier, 0f, input.z * _moveSpeedMultyplier);
+        //двигаем
+        _body.AddForce(_moveDirection);
+    }
+
+    private void Jump(bool jump)
+    {
+        if (jump) _body.AddForce(Vector3.up * 250f);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+//не совсем верная реализация
+public class PlayerInput : MonoBehaviour
+{
+
+    [SerializeField] private string _horizontalAxis = "Horizontal";
+    [SerializeField] private string _verticalAxis = "Vertical";
+
+    private Vector3 _inputAxis;
+
+    public static Action<Vector3> OnInput;
+    public static Action<Boolean> OnInputSpaceBtn;
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis(_horizontalAxis);
+        float vertical = Input.GetAxis(_verticalAxis);
+
+        _inputAxis.Set(horizontal, 0f, vertical);
+
+        //всем ктл подписался на событие OnInput отдали _inputAxis
+        OnInput?.Invoke(_inputAxis);
+    }
+
+    private void Update()
+    {
+        OnInputSpaceBtn?.Invoke(Input.GetKeyDown(KeyCode.Space));
+    }
+
+}
