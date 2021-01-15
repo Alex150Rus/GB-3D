7ecfadd933826477ef1e95d0195820ac8b37a04b
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-01-15 13:06:19 +0300

    added door in level

--- Assets/Scripts/Doors/Door1L.cs
+++ Assets/Scripts/Doors/Door1L.cs
@@ -1,19 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Door1L : MonoBehaviour
 
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform _openableDoor;
+    [SerializeField] private bool _keyNeeded = false;
+    [SerializeField] private bool _opened = false;
+    [SerializeField] private string[] _tagsToReact = { "Player", "Enemy", };
+
+    private GameObject _collisionGameObject;
+    private enum State
     {
-        
+        Closed,
+        Opened,
+    }
+   private State _state;
+
+    private void Start()
+    {
+        _state = _opened ? State.Opened : State.Closed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        _collisionGameObject = other.gameObject;
+        foreach (string tag in _tagsToReact)
+        {
+            if (_collisionGameObject.CompareTag(tag))
+            {
+                OpenDoor();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (string tag in _tagsToReact)
+        {
+            if (_collisionGameObject.CompareTag(tag))
+            {
+                CloseDoor();
+            }
+        }
+    }
+
+    private void CloseDoor()
+    {
+        if (_state == State.Opened)
+        {
+            _openableDoor.gameObject.SetActive(true);
+            _state = State.Closed;
+        }
+    }
+
+    private void OpenDoor()
+    {
+        if (_state == State.Closed && ValidateKey())
+        {
+            _openableDoor.gameObject.SetActive(false);
+            _state = State.Opened;
+        }
+    }
+
+   private bool ValidateKey()
+    {
+        if (_keyNeeded)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
