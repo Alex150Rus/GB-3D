379917b268c16b6d79334cfc8228e886e4872c6d
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-01-16 18:07:24 +0300

    added weapon: Thrower script

---
+++ Assets/Scripts/Weapons/Thrower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour
+{
+    [SerializeField] GameObject _prefab;
+    [SerializeField] int _throwForce = 200;
+    [SerializeField] int _qtyOfAmmo = 10;
+
+
+    void Start()
+    {
+        PlayerInput.OnInputFireBtn2 += ThrowBall;
+    }
+
+    private void ThrowBall(bool throwBall)
+    {
+        if(throwBall && _qtyOfAmmo > 0)
+        {
+            GameObject ball = Instantiate(_prefab, transform.position, Quaternion.identity);       
+            ball.GetComponent<Rigidbody>().AddForce(transform.forward * _throwForce);
+            _qtyOfAmmo--;
+        }
+    }
+}
