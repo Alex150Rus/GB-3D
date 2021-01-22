f552d0388775b6a09dc2a4ce8f2fdbf47549e11c
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-01-22 11:25:18 +0300

    prefabs and turrel script work

--- Assets/Scripts/Weapons/Turrel.cs
+++ Assets/Scripts/Weapons/Turrel.cs
@@ -7,58 +7,30 @@ public class Turrel : MonoBehaviour
     [SerializeField] private GameObject _prefab;
     [SerializeField] private Transform _enemy;
     [SerializeField] private float _throwForce= 50;
-    [SerializeField] private float _distance = 23;
     [SerializeField] private float _fireIntervalInSeconds = 1f;
-    [SerializeField] private float _ballStartHeight = 0.8f;
 
-    private Vector3 _ballStartPosition;
+    private bool _isFiring = false;
     
-    private enum State
-    {
-        TargetIsInvisible,
-        TargetIsVisible,
-    }
-
-    private State _state;
-
-
     private void Start()
     {
-        _state = State.TargetIsInvisible;
-        _ballStartPosition.Set(transform.position.x, transform.position.y + _ballStartHeight, transform.position.z);
-
-        StartCoroutine(Fire());
+       StartCoroutine(Fire());
     }
 
-    private void Update()
+    public void StartFiring(bool isFiring)
     {
-        FindEnemy();
+        _isFiring = isFiring;
     }
 
-    private void FindEnemy()
-    {
-        RaycastHit hit;
-
-        Vector3 direction = (_enemy.transform.position-transform.position).normalized;
-        if (Physics.Raycast(transform.position, direction, out hit, _distance) && hit.collider.CompareTag(_enemy.tag) )
-        {
-            transform.LookAt(_enemy.position);
-            _state = State.TargetIsVisible;
-        } else
-        {
-            _state = State.TargetIsInvisible;
-        }
-    }
 
     private IEnumerator Fire()
     {
         while (true)
         {
-            while (_state == State.TargetIsVisible)
+            while (_isFiring)
             {
                 
-                GameObject ball = Instantiate(_prefab, _ballStartPosition, Quaternion.identity);
-                ball.GetComponent<Rigidbody>().AddForce((_enemy.transform.position - transform.position) * _throwForce);
+                GameObject ball = Instantiate(_prefab, transform.position, Quaternion.identity);
+                ball.GetComponent<Rigidbody>().AddForce((_enemy.transform.position - transform.position + Vector3.up) * _throwForce);
                 yield return new WaitForSeconds(_fireIntervalInSeconds);
             }
             yield return null;
