d6ab54fa21000b6181b68469c9923ac9bede976b
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-01-22 11:26:18 +0300

    Enemy and turrel ball(ammo) script work

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform _enemy;
+    [SerializeField] private int _enemyDistance = 23;
+    [SerializeField] private UnityEvent onTargetIsVisible;
+    [SerializeField] private UnityEvent onTargetIsInvisible;
+
+    private void Update()
+    {
+        FindEnemy();
+    }
+
+    private void FindEnemy()
+    {
+        RaycastHit hit;
+
+        Vector3 direction = _enemy.transform.position - transform.position;
+      
+        if (Physics.Raycast(transform.position, direction, out hit, _enemyDistance) && hit.collider.CompareTag(_enemy.tag))
+        {
+            transform.LookAt(_enemy.position);
+            onTargetIsVisible?.Invoke();            
+        } else onTargetIsInvisible?.Invoke();
+    }
+
+}
---
+++ Assets/Scripts/Weapons/Ammo/TurrelBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrelBall : MonoBehaviour
+{
+    [SerializeField] private int _damage = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) collision.gameObject.GetComponent<Health>().TakeDamage(_damage);
+
+        Debug.Log(collision.gameObject);
+        Destroy(gameObject);
+    }
+
+}
