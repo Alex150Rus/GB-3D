5d13d473ea7e84e9ca8761deb61c082d85732dae
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-01-26 13:49:47 +0300

    we can Pause game now

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _GameIsPaused;
+    [SerializeField] private UnityEvent _GameIsResumed;
+
+    private bool _gamePaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerInput.OnInputEscape += PauseGame;
+    }
+
+    private void PauseGame(bool input)
+    {
+        if (input && !_gamePaused) { 
+            Time.timeScale = 0;
+            _gamePaused = true;
+            _GameIsPaused?.Invoke();
+        } else if(input)
+        {
+            _gamePaused = false;
+            ResumeGame();
+            _GameIsResumed?.Invoke();
+        }
+    }
+
+    private void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -9,14 +9,17 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private string _verticalAxis = "Vertical";
     [SerializeField] private string _fireBtn1 = "Fire1";
     [SerializeField] private string _fireBtn2 = "Fire2";
+    [SerializeField] private string _cancel = "Cancel";
 
     private Vector3 _inputAxis;
+    private bool _takingInput = true;
 
     public static Action<Vector3> OnInput;
     public static Action<Boolean> OnInputSpaceBtn;
     public static Action<Boolean> OnInputFireBtn;
     public static Action<Boolean> OnInputFireBtn2;
     public static Action<Boolean> OnInputQ;
+    public static Action<Boolean> OnInputEscape;
     public static Action<Boolean, Vector3> OnInputMMB;
 
     private void FixedUpdate()
@@ -33,11 +36,21 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        OnInputSpaceBtn?.Invoke(Input.GetKeyDown(KeyCode.Space));
-        OnInputFireBtn?.Invoke(Input.GetButtonDown(_fireBtn2));
-        OnInputFireBtn2?.Invoke(Input.GetButtonDown(_fireBtn1));
-        OnInputQ?.Invoke(Input.GetKeyDown(KeyCode.Q));
-        OnInputMMB?.Invoke(Input.GetMouseButton(2), new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0));
-}
+        if (_takingInput)
+        {
+            OnInputSpaceBtn?.Invoke(Input.GetKeyDown(KeyCode.Space));
+            OnInputFireBtn?.Invoke(Input.GetButtonDown(_fireBtn2));
+            OnInputFireBtn2?.Invoke(Input.GetButtonDown(_fireBtn1));
+            OnInputQ?.Invoke(Input.GetKeyDown(KeyCode.Q));
+            OnInputMMB?.Invoke(Input.GetMouseButton(2), new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0));
+        }
+
+        OnInputEscape?.Invoke(Input.GetButtonDown(_cancel));
+    }
+
+    public void TakingInput(bool takingItput)
+    {
+        _takingInput = takingItput;
+    }
 
 }
