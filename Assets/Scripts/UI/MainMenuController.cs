54babdb8df4abdeb36d84d0c59efb305e339110e
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-01-26 12:57:49 +0300

    created corridors and rooms prefabs, levels menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -9,15 +9,18 @@ public class MainMenuController : MonoBehaviour
     {
         Main,
         Settings,
+        Levels,
     }
 
     public CanvasGroup mainScreen;
     public CanvasGroup settingsScreen;
+    public CanvasGroup levelsScreen;
 
     void SetCurrentScreen(Screen screen)
     {
         Utility.SetCanvasGroupEnabled(mainScreen, screen == Screen.Main);
         Utility.SetCanvasGroupEnabled(settingsScreen, screen == Screen.Settings);
+        Utility.SetCanvasGroupEnabled(levelsScreen, screen == Screen.Levels);
     }
 
     // Start is called before the first frame update
@@ -31,9 +34,19 @@ public class MainMenuController : MonoBehaviour
         SceneManager.LoadScene("Level1");
     }
 
+    public void StartLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
     public void OpenSettings()
     {
         SetCurrentScreen(Screen.Settings);
+    }
+
+    public void OpenLevelMenu()
+    {
+        SetCurrentScreen(Screen.Levels);
     }
 
     public void CloseSettings()
