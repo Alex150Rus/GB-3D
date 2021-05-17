c62e5afec9157c34709730d7b3f00378a0ec61fc
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-05-17 03:22:45 +0300

    moved prefabs folder to Resources folder, created Application script and assigned it to the same name Game Object

---
+++ Assets/Scripts/Application.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Application : MonoBehaviour
+    {
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FixedUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDestroy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
